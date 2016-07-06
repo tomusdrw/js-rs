66d17ff88e90e389c3d9498a5af968be553e6470
Tomusdrw <tomusdrw@gmail.com>
2016-07-06 16:16:58 +0200

    Initial version

--- build.rs
+++ build.rs
@@ -1,4 +1,21 @@
+use std::fmt;
+use std::process::Command;
+
+fn die<T : fmt::Debug>(s: &'static str, e: T) -> ! {
+  panic!("Error: {}: {:?}", s, e);
+}
 
 fn main () {
+  let mut child = Command::new("npm")
+    .arg("run")
+    .arg("build")
+    .env("NODE_ENV", "production")
+    .current_dir(concat!(env!("CARGO_MANIFEST_DIR"), "/web"))
+    .spawn()
+    .unwrap_or_else(|e| die("Running command", e));
 
+  println!("Building JS code: Started");
+  let code = child.wait().unwrap_or_else(|e| die("Building JS code", e));
+  assert!(code.success());
+  println!("Building JS code: Finished");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::path;
-
 fn main() {
     println!(
       "{}",
